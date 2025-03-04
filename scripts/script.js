48ef6c1ba2ece89100d3094a9810a01c1db1b494
Niki <87618287+Zneziz@users.noreply.github.com>
2025-03-03 23:38:37 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -7,28 +7,19 @@ hamburger.addEventListener("click", () => nav.classList.toggle("active"));
 
 
 //rolagem dinamica
-const navRedes = document.querySelector("#nav-redes");
 const header = document.querySelector('.header')
 
 window.addEventListener("scroll", function() {
     if (window.scrollY > 60) {
-        navRedes.classList.remove('nav-redes');
-        navRedes.classList.add('nav-redes-none');
-
-        hamburger.classList.remove('hamburger');
-        hamburger.classList.add('hamburger-roll');
+    
+        header.classList.remove('header-untranslatey')
+        header.classList.add('header-translatey')
     } else {
-        navRedes.classList.remove('nav-redes-none');
-        navRedes.classList.add('nav-redes');
-
-        hamburger.classList.remove('hamburger-roll');
-        hamburger.classList.add('hamburger');
+       
+        header.classList.remove('header-translatey')
+        header.classList.add('header-untranslatey')
     }
 });
 
-//window.addEventListener("scroll", function() {
-//    if (window.scrollY = 20) {
-//        header.classList.toggle('header-translatey')
-//   }
-//});
+
 
